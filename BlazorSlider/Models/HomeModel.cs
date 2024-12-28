e8026088b293e703ca703530d0c90d204cc4aaec
Tore Aurstad <tore.aurstad@hemit.no>
2024-12-28 23:55:18 +0100

    Work on showing the ticks

---
+++ BlazorSlider/Models/HomeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorSlider.Models
+{
+    public class HomeModel
+    {
+        public HomeDataContract Data { get; set; } = new();
+
+        public class HomeDataContract
+        {
+            public int Eq5dq6 { get; set; }
+        }
+    }
+}
